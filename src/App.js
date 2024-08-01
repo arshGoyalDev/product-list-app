1ba3c0d247c45f95765dddfb0234b91b5e390e55
arshGoyalDev <arshgoyalDev@gmail.com>
2024-08-01 21:21:06 +0530

    add: empty cart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import productsData from "./assets/products.json";
 
 import ProductsContainer from "./Components/ProductsContainer";
+import Cart from "./Components/Cart";
 
 const App = () => {
   const [cart, updateCart] = useState([]);
 
-  useEffect(() => {
-    console.log(cart)
-  }, [cart])
-
   return (
-    <main className="bg-neutral-rose50 min-h-screen px-8 py-10 md:p-16 lg:p-20 xl:p-28">
+    <main className="grid gap-7 lg:grid-cols-3 xl:grid-cols-4 bg-neutral-rose50 min-h-screen px-8 py-10 md:p-16 lg:p-[75xp] xl:p-[90px] xxl:p-28">
       <ProductsContainer
         productsData={productsData}
         cart={cart}
         updateCart={updateCart}
       />
+      <Cart cart={cart} updateCart={updateCart} />
     </main>
   );
 };
---
+++ src/Components/Cart.js
@@ -0,0 +1,15 @@
+const Cart = () => {
+  return (
+    <section className="bg-white rounded-xl p-6 xl:p-7 grid gap-10 h-fit">
+      <h1 className="text-2xl text-primary-red font-extrabold">Your Cart (0)</h1>
+      <div>
+        
+        <div className="flex flex-col items-center justify-center gap-5 pb-5">
+          <img src="/images/illustration-empty-cart.svg" alt="illustration empty cart" />
+          <p className="text-neutral-rose500 font-medium">Your added items will display here</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Cart
