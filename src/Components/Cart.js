01b8b4767d4a2ce6ac8a94eef988fb2e6cd1ad69
arshGoyalDev <arshgoyalDev@gmail.com>
2024-08-06 21:14:34 +0530

    fix: cart overflow

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -15,7 +15,7 @@ const Cart = ({ cart, updateCart }) => {
   };
 
   return (
-    <section className="bg-white rounded-xl p-6 xl:p-7 grid h-fit lg:fixed lg:top-[75px] lg:right-[75px] xl:top-[70px] xl:right-[70px] xxl:top-28 xxl:right-28 lg:w-[35%] xl:w-[28%] xxl:w-[25%]">
+    <section className="bg-white rounded-xl p-6 xl:p-7 grid h-fit lg:fixed lg:top-[75px] lg:right-[75px] xl:top-[70px] xl:right-[70px] xxl:top-28 xxl:right-28 lg:w-[35%] xl:w-[28%] xxl:w-[25%] lg:max-h-[70%] overflow-y-auto">
       <h1 className="text-2xl text-primary-red font-extrabold">
         Your Cart ({getTotalNumber()})
       </h1>
